f13e836688ea1b775348a5d5107557636ba598b1
Henrique Nogueira <henrique@darkglass.com>
2024-06-12 10:17:51 +0300

    update 11.6

--- index.jsx
+++ index.jsx
@@ -1,18 +1,4 @@
-import React from 'react'
 
-var cham = {
-    title: 'Real',
-    subtitle:'Fake',
-}
 
-var template = (
-  <div>
-    <h1>{cham.title}</h1>
-    <p>{cham.subtitle}</p>
-    <ol>
-      <li>Item One</li>
-      <li>Item Two</li>
-    </ol>
-  </div>
-)
+
 
--- src/app.js
+++ src/app.js
@@ -3,6 +3,22 @@ import {createRoot} from 'react-dom/client'
 
 console.log('App.js is running!')
 
+var cham = {
+  title: 'Real',
+  subtitle:'Fake',
+}
+
+var template = (
+<div>
+  <h1>{cham.title}</h1>
+  <p>{cham.subtitle}</p>
+  <ol>
+    <li>Item One</li>
+    <li>Item Two</li>
+  </ol>
+</div>
+)
+
 var user = {
   age: 21, 
 }
